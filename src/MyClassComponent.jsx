b87f854e038c6debd2478528570fa087e826bd7e
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2020-08-12 13:10:24 +0100

    ql4 - class components

---
+++ src/MyClassComponent.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class MyClassComponent extends Component {
+
+    render() {
+        return (
+            <>
+                <h2>Class Components</h2>
+                <p>Work in a similar way to Function Components</p>
+            </>
+        );
+    }
+}
+
+export default MyClassComponent;
